7487997ea972a8ee10e6abc909ecc1ee51d75e74
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2024-06-01 16:17:56 +0900

    Japanese keyboard

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import {
-  layer,
   map,
   NumberKeyValue,
   rule,
@@ -7,11 +6,27 @@ import {
   writeToProfile,
 } from 'karabiner.ts'
 
-writeToProfile('tobark', [
-  rule('R Command to Meh').manipulators([map('right_command').toMeh()]),
-  rule('Meh asdf to arrow keys').manipulators([
-    map('a', 'Meh').to('left_arrow'),
-    map('s', 'Meh').to('up_arrow'),
+writeToProfile('hyunjoon - magic keyboard japanese', [
+  rule('Japanese keys').manipulators([
+    map('japanese_eisuu', 'optionalAny').to('left_command'),
+    map('left_command', 'optionalAny').to('left_option'),
+    map('japanese_kana', 'optionalAny').toMeh(),
+  ]),
+
+  rule('Number keys').manipulators([
+    map('1', 'optionalAny').to('grave_accent_and_tilde'),
+    //          1, 2, 3, 4, 5, 6, 7, 8, 9
+    withMapper([2, 3, 4, 5, 6, 7, 8, 9, 0])((k, i) =>
+      map(k as NumberKeyValue, 'optionalAny').to((i + 1) as NumberKeyValue)
+    ),
+    map('-', 'optionalAny').to('0'),
+    map('=', 'optionalAny').to('hyphen'),
+    map('international3', 'optionalAny').to('equal_sign'),
+  ]),
+
+  rule('Meh esdf to arrow keys').manipulators([
+    map('s', 'Meh').to('left_arrow'),
+    map('e', 'Meh').to('up_arrow'),
     map('d', 'Meh').to('down_arrow'),
     map('f', 'Meh').to('right_arrow'),
   ]),
