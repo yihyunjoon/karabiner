ff62fab82fd0afab8abeacd0cc4e3f6439598c05
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2024-06-17 14:15:29 +0900

    internal keyboard

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { map, rule, writeToProfile } from 'karabiner.ts'
+import { map, rule, toKey, writeToProfile } from 'karabiner.ts'
 import { cursorMotionRules, numberKeysRule, progressiveRule } from './rules'
 
 writeToProfile('hyunjoon - magic keyboard japanese', [
@@ -17,9 +17,20 @@ writeToProfile('hyunjoon - magic keyboard japanese progressive', [
     map('left_command', 'optionalAny').to('left_option'),
     map('japanese_kana', 'optionalAny').to('spacebar'),
     map('right_command', 'optionalAny').toMeh(),
-    map('spacebar', 'optionalAny').toMeh(),
+    map('spacebar', 'optionalAny')
+      .toIfAlone('spacebar', {}, { halt: true })
+      .toDelayedAction(toKey('vk_none'), toKey('spacebar'))
+      .toIfHeldDown('l⇧', 'l⌥⌃', { halt: true })
+      .parameters({ 'basic.to_if_held_down_threshold_milliseconds': 220 }),
   ]),
-
   numberKeysRule,
+  cursorMotionRules,
+])
+
+writeToProfile('internal keyboard', [
+  rule('internal keyboard').manipulators([
+    map('right_command', 'optionalAny').toMeh(),
+    map('caps_lock', 'optionalAny').to('left_control'),
+  ]),
   cursorMotionRules,
 ])
